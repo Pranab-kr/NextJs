c8020a81f3612fbd5d92e591c10dea940dcdbc28
Pranab-kr <pranabm406@gmail.com>
2025-12-18 23:55:26 +0530

    Change ToastProvider position to bottom-right

--- 07-server Actions and optimizations/05-contact-form-project/app/layout.tsx
+++ 07-server Actions and optimizations/05-contact-form-project/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ToastProvider position="top-right">
+        <ToastProvider position="bottom-right">
           <AnchoredToastProvider>
             <ThemeProvider
               attribute="class"
